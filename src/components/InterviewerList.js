c0402cf6359afe704eb057e0ff6ed51535822fef
angelareit <anreit01@gmail.com>
2023-04-23 23:13:49 -0400

    added axios and implemented a scheduler api call to get days on application.js

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "components/InterviewerList.scss"
+import InterviewerListItem from "./InterviewerListItem";
+
+export default function InterviewerList(props) {
+  console.log('PROPS.SETINTERVIWER:', props.onChange);
+  const interviewerElements = props.interviewers.map((interviewer) => {
+    return <InterviewerListItem
+      key={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.value}
+      setInterviewer={()=>props.onChange(interviewer)}    
+      />
+  });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewerElements}</ul>
+    </section>
+  );
+}
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import classNames from "classnames";
+
+import "components/InterviewerListItem.scss";
+
+
+export default function InterviewerListItem(props) {
+  let buttonClass = classNames('interviewers__item', { 'interviewers__item--selected ': props.selected });
+  console.log('PROPS.setinterviewer:', props.setInterviewer);
+
+  return (
+    <li className={buttonClass} onClick={props.setInterviewer
+    }>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
